570c686ddf1d0f628aa984cd6332be96dff68a9e
gustavo <gustavo.isaac@gmail.com>
2021-10-17 15:53:24 +0100

    Project setup

--- app/src/main/java/com/artemissoftware/irisnotes/ui/theme/Theme.kt
+++ app/src/main/java/com/artemissoftware/irisnotes/ui/theme/Theme.kt
@@ -5,13 +5,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
 )
-
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
@@ -28,7 +30,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun IrisNotesTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun IrisNotesTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
