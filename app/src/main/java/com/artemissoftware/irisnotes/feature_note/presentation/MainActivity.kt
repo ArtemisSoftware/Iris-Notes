a2f05b882a65633407d5b01a2383620a18987ca0
gustavo <gustavo.isaac@gmail.com>
2021-10-18 10:08:48 +0100

    Navigation setup

--- app/src/main/java/com/artemissoftware/irisnotes/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/irisnotes/feature_note/presentation/MainActivity.kt
@@ -10,29 +10,75 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.artemissoftware.irisnotes.ui.theme.IrisNotesTheme
 
+import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.artemissoftware.irisnotes.feature_note.presentation.add_edit_note.AddEditNoteScreen
+import com.artemissoftware.irisnotes.feature_note.presentation.notes.NotesScreen
+import com.artemissoftware.irisnotes.feature_note.presentation.util.Screen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             IrisNotesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.NotesScreen.route
+                    ) {
+                        composable(route = Screen.NotesScreen.route) {
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNoteScreen.route +
+                                    "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                            )
+                        ) {
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(
+                                navController = navController,
+                                noteColor = color
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IrisNotesTheme {
-        Greeting("Android")
+
     }
 }
--- app/src/main/java/com/artemissoftware/irisnotes/feature_note/presentation/notes/components/DefaultRadioButton.kt
+++ app/src/main/java/com/artemissoftware/irisnotes/feature_note/presentation/notes/components/DefaultRadioButton.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.artemissoftware.irisnotes.feature_note.presentation.Greeting
 import com.artemissoftware.irisnotes.ui.theme.IrisNotesTheme
 
 @Composable
